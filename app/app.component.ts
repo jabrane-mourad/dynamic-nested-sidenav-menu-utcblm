d0d3551b3d6b50dc3f2722a3497c9554ef0c2659
jabrane-mourad <mouradjabrane1998@gmail.com>
2021-01-03 21:03:15 +0100

    Initial commit

---
+++ app/app.component.ts
@@ -0,0 +1,116 @@
+import {
+  Component,
+  ViewChild,
+  ElementRef,
+  ViewEncapsulation,
+  AfterViewInit
+} from "@angular/core";
+import { VERSION } from "@angular/material/core";
+import { NavItem } from "./nav-item";
+import { NavService } from "./nav.service";
+
+@Component({
+  selector: "material-app",
+  templateUrl: "app.component.html",
+  styleUrls: ["app.component.scss"],
+  encapsulation: ViewEncapsulation.None
+})
+export class AppComponent implements AfterViewInit {
+  @ViewChild("appDrawer") appDrawer: ElementRef;
+  version = VERSION;
+  navItems: NavItem[] = [
+    {
+      displayName: "DevFestFL",
+      iconName: "recent_actors",
+      route: "devfestfl",
+      children: [
+        {
+          displayName: "Speakers",
+          iconName: "group",
+          route: "devfestfl/speakers",
+          children: [
+            {
+              displayName: "Michael Prentice",
+              iconName: "person",
+              route: "devfestfl/speakers/michael-prentice",
+              children: [
+                {
+                  displayName: "Create Enterprise UIs",
+                  iconName: "star_rate",
+                  route: "devfestfl/speakers/michael-prentice/material-design"
+                }
+              ]
+            },
+            {
+              displayName: "Stephen Fluin",
+              iconName: "person",
+              route: "devfestfl/speakers/stephen-fluin",
+              children: [
+                {
+                  displayName: "What's up with the Web?",
+                  iconName: "star_rate",
+                  route: "devfestfl/speakers/stephen-fluin/what-up-web"
+                }
+              ]
+            },
+            {
+              displayName: "Mike Brocchi",
+              iconName: "person",
+              route: "devfestfl/speakers/mike-brocchi",
+              children: [
+                {
+                  displayName: "My ally, the CLI",
+                  iconName: "star_rate",
+                  route: "devfestfl/speakers/mike-brocchi/my-ally-cli"
+                },
+                {
+                  displayName: "Become an Angular Tailor",
+                  iconName: "star_rate",
+                  route: "devfestfl/speakers/mike-brocchi/become-angular-tailor"
+                }
+              ]
+            }
+          ]
+        },
+        {
+          displayName: "Sessions",
+          iconName: "speaker_notes",
+          route: "devfestfl/sessions",
+          children: [
+            {
+              displayName: "Create Enterprise UIs",
+              iconName: "star_rate",
+              route: "devfestfl/sessions/material-design"
+            },
+            {
+              displayName: "What's up with the Web?",
+              iconName: "star_rate",
+              route: "devfestfl/sessions/what-up-web"
+            },
+            {
+              displayName: "My ally, the CLI",
+              iconName: "star_rate",
+              route: "devfestfl/sessions/my-ally-cli"
+            },
+            {
+              displayName: "Become an Angular Tailor",
+              iconName: "star_rate",
+              route: "devfestfl/sessions/become-angular-tailor"
+            }
+          ]
+        },
+        {
+          displayName: "Feedback",
+          iconName: "feedback",
+          route: "devfestfl/feedback"
+        }
+      ]
+    }
+  ];
+
+  constructor(private navService: NavService) {}
+
+  ngAfterViewInit() {
+    this.navService.appDrawer = this.appDrawer;
+  }
+}
